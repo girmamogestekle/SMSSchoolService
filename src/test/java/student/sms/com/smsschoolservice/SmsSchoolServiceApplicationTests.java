5ffc2143daa4274668613e3605eaaf33d5cc2df1
GirmaMogesTekle <girma.moges.teklemariam@outlook.com>
2025-09-26 17:12:31 -0500

    Add project files

---
+++ src/test/java/student/sms/com/smsschoolservice/SmsSchoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package student.sms.com.smsschoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsSchoolServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
